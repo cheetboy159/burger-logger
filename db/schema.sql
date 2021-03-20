194959b55be0253141ba56d0c1819b59920e49fc
Mena Wahba <menawahba@gmail.com>
2021-03-20 18:45:00 -0400

    fixed my layout

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
-### Schema
-
+DROP DATABASE IF EXISTS burger_db;
 CREATE DATABASE burger_db;
 USE burger_db;
 
@@ -7,6 +6,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR (100) NOT NULL,
-    devoured BOOLEAN DEFAULT fal
+    devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
